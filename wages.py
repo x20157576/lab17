f5bc092f338727dd71aeb28e420792e3ae6fc8f0
x20157576 <x20157576@student.ncirl.ie>
2020-10-17 21:48:15 +0100

    added wages.py

---
+++ wages.py
@@ -0,0 +1,22 @@
+def weeklyDosh(hours, wage):
+    if hours > 40:
+        return 40 * wage + (hours - 40) * wage * 1.5
+    else:
+        return hours * wage
+
+
+def getInput(usrinput):
+        return float(input(usrinput))
+
+
+hours = getInput("How many hours did you work this week? ")
+wage = getInput("What is your hourly rate? ")
+
+pay = weeklyDosh(hours, wage)
+
+print(f"Total pay: ${pay:.2f} ")
+
+if hours > 40:
+    print('and you did {} hours overtime.'.format(int(hours)))
+
+
